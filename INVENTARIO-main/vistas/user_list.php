de35d47f9e08f3fd9af161e3592ec9cda379b318
Manuel <manuelbarria15@gmail.com>
2022-11-09 23:26:54 -0500

    Update user_list.php

--- INVENTARIO-main/vistas/user_list.php
+++ INVENTARIO-main/vistas/user_list.php
@@ -1,6 +1,6 @@
 <div class="container is-fluid mb-6">
     <h1 class="title">Usuarios</h1>
-    <h2 class="subtitle">Lista de usuarios+*</h2>
+    <h2 class="subtitle">Lista de usuarios</h2>
 </div>
 
 <div class="container pb-6 pt-6">
